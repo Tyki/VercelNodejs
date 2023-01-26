5350017e81cf5c1f6ba33cbcd07300972f7a70a2
bleulliette <bleulliette@kaliop.com>
2023-01-26 15:11:48 +0100

    restdb

--- index.js
+++ index.js
@@ -1,24 +1,36 @@
 // index.js
+
+const axios = require("axios");
 const express = require("express");
-
 const app = express();
-const PORT = 4000;
 
-app.listen(PORT, () => {
-  console.log(`API listening on PORT ${PORT} `);
+app.get("/myspecialusers", async function (req, res) {
+  const fetchUser1 = axios.get("https://jsonplaceholder.typicode.com/users/1");
+  const fetchUser4 = axios.get("https://jsonplaceholder.typicode.com/users/4");
+  const fetchUser6 = axios.get("https://jsonplaceholder.typicode.com/users/6");
+
+  const [response1, response4, response6] = await Promise.all([
+    fetchUser1,
+    fetchUser4,
+    fetchUser6,
+  ]);
+
+  await axios({
+    method: "post",
+    url: `https://testdemo-c2ca.restdb.io/rest/test`,
+    data: {
+      user: [response1.data],
+    },
+    headers: {
+      "x-apikey": "fb449cc964ef6fc07b1975476a575f11fdaba",
+    },
+  });
+
+  res.send({
+    responses: [response1.data, response4.data, response6.data],
+  });
 });
 
-app.get("/", (req, res) => {
-  res.send("Hey this is my API running 🥳");
+app.listen(3000, function () {
+  console.log("Example app listening on port 3000!");
 });
-
-app.get("/about", (req, res) => {
-  res.send("This is my about route..... ");
-});
-
-app.post("/test", (req, res) => {
-  res.send(req.body);
-});
-
-// Export the Express API
-module.exports = app;
